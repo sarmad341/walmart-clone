60d1fbcbc4b4b7b367d85aafd6eb98695c84febe
Dell <muhammadsarmad2003@gmail.com>
2025-07-14 21:20:59 +0500

    feat: completed main page layout structure

--- components/GridOption.tsx
+++ components/GridOption.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 
 type Props = {
@@ -10,11 +12,21 @@ function GridOption({ title, className, image }: Props) {
   return (
     <Link
       href={{
-        pathname: "/",
+        pathname: "/search",
         query: { q: title },
       }}
+      className={cn("grid-option relative", className)}
     >
-      <h2>{title}</h2>
+      <h2 className="text-xl font-bold">{title}</h2>
+
+      {image && (
+        <Image
+          src={image}
+          alt={title}
+          layout="fill"
+          className="object-cover opacity-20 rounded-md"
+        />
+      )}
     </Link>
   );
 }
