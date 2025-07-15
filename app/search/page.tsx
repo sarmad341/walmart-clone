83efc75fff7a07acf28ffbde95197cd9d9f0acfb
Dell <muhammadsarmad2003@gmail.com>
2025-07-15 20:29:58 +0500

    feat: integrated search functionality using Oxylabs Web Scraper API

---
+++ app/search/page.tsx
@@ -0,0 +1,30 @@
+import fetchSearch from "@/lib/fetchSearch";
+
+type Props = {
+  searchParams: {
+    q: string;
+  };
+};
+
+async function Searchpage({ searchParams: { q } }: Props) {
+  const results = await fetchSearch(q);
+
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-2">Results for {q}</h1>
+      <h2 className="mb-5 text-gray-400">
+        ({results?.content.total_results} results)
+      </h2>
+
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {results?.content.organic.map((product) => (
+          <li key={product.product_id}>
+            <p>{product.title}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Searchpage;
